080fe7f6fef0ea2a0f427d437b85af379d09f0d8
Graeme Maciver <graeme@Graemes-MacBook-Pro.local>
2014-11-22 18:45:24 +0000

    Short theme version 1

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var Handlebars = require("handlebars");
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+	// var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+	// Nicer dates
+	Handlebars.registerHelper('date', function(date) {
+	  var theDate = new Date(date);
+
+	  return months[theDate.getMonth()] + ' ' + theDate.getFullYear();
+	});
+
 	return Handlebars.compile(template)({
 		css: css,
 		resume: resume
