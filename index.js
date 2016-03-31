e0c130ebf91044d7e697d589bf1a85a7126d65fe
Graeme Maciver <me@graememaciver.com>
2016-03-31 16:02:26 +0100

    pragraphs on summary text

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ function render(resume) {
 	});
 }
 
+Handlebars.registerHelper('paragraphSplit', function(plaintext) {
+    var i, output = '',
+        lines = plaintext.split(/\r\n|\r|\n/g);
+    for (i = 0; i < lines.length; i++) {
+        if(lines[i]) {
+            output += '<p>' + lines[i] + '</p>';
+        }
+    }
+    return new Handlebars.SafeString(output);
+});
+
 module.exports = {
 	render: render
 };
